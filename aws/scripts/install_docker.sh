42346b642d06d8af785e131c411b11500bea7846
Luis Martins <luismwolf@gmail.com>
2024-03-13 11:17:31 -0400

    make install docker script silent

--- aws/scripts/install_docker.sh
+++ aws/scripts/install_docker.sh
@@ -5,8 +5,8 @@ if [ -x "$(command -v docker)" ]; then
 else 
     echo Installing docker.
     # Add Docker's official GPG key:
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt-get -yqq update 
+    sudo apt-get -yqq install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -16,9 +16,9 @@ else
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
+    sudo apt-get -yqq update
 
     # Install docker
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get -yqq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 # REF: https://stackoverflow.com/questions/46575136/how-to-check-if-docker-is-installed-in-a-unix-shell-script
